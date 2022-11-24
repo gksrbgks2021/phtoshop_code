424f303302987932b2f5324c5f3544ad9476c1f6
gksrbgks2021 <gksrbgks2021@gmail.com>
2022-11-25 04:18:51 +0900

    add dock widget

--- photoshop.py
+++ photoshop.py
@@ -1,50 +1,72 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QCoreApplication
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import *
+import numpy as np, cv2
+import matplotlib.pyplot as plt
 import os
 
 class MyApp(QMainWindow):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)        
-        
+        self.img_original  = np.zeros((3,3),np.uint8)
+        self.widget_cnt = 0
+
         self.initUI()
         
-    def initUI(self):    
-        
-        self.setGeometry(200, 200, 200, 200)           
-        
-        exitAction = QAction('&Exit', self)        
-        exitAction.setShortcut('Esc') #단축키 추가 메소드
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
+    def initUI(self): 
 
-        self.statusBar()
+        self.setWindowIcon(QtGui.QIcon('./img/cat.jpg'))#이미지
+        self.setStyleSheet('background : lightgray;')
+        self.setGeometry(250, 130, 1300, 850)    
+        self.add_btn() #버튼 추가
+        self.add_menubar()#메뉴바 추가
+        self.setWindowTitle('PyQt5 포토샵 구현')#제목 설정
+        self.layout = QVBoxLayout() #수직 레이아웃 설정
+        self.setLayout(self.layout)
 
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAction)
-        
-        self.setWindowTitle('PyQt5 Basic Menubar')    
-        self.show()
+        self.show() #화면에 띄우기
 
     #버튼 추가
     def add_btn(self):
+        #불러오기 버튼 
         img_path_btn = QPushButton('불러오기', self)
         img_path_btn.resize(img_path_btn.sizeHint())
         img_path_btn.setToolTip('이미지를 불러옵니다')
-        img_path_btn.clicked.connect(QCoreApplication.instance().quit) #버튼 이벤트 처리 
+        img_path_btn.clicked.connect(self.openfile) #파일 불러오기 메소드
         img_path_btn.move(50, 50) #위치이동
-    
+
+        img_path_btn = QPushButton('원본 이미지 보기', self)
+        img_path_btn.resize(img_path_btn.sizeHint())
+        img_path_btn.setToolTip('이미지를 불러옵니다')
+        img_path_btn.clicked.connect(self.show_img_original) #버튼 이벤트 처리 
+        img_path_btn.move(150, 50) #위치이동
+
+        img_path_btn = QPushButton('저 장', self)
+        img_path_btn.resize(img_path_btn.sizeHint())
+        img_path_btn.setToolTip('이미지를 불러옵니다')
+        img_path_btn.clicked.connect(self.savefile) #버튼 이벤트 처리 
+        img_path_btn.move(1200, 770) #가로, 세로
+        
+    def add_widget(self):
+        img_widget = ImageWidget()
+        self.flayout = QtWidgets.QFormLayout(img_widget)
+        self.setCentralWidget(img_widget)
+        
+    #QlineEdit 추가
+    def add_QlineEdit(self):
+        print('s')
+
     #메뉴바 추가
     def add_menubar(self):
         exitAction = QAction('&Exit', self)        
-        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setShortcut('Esc')#강제종료 키 esc
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
 
         self.statusBar()
+
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
@@ -58,7 +80,56 @@ class MyApp(QMainWindow):
             self.save_file()
 
         event.accept()
-        
+
+    def add_statusbar(self):#상태바 추가 
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+    def openfile(self): #파일 불러오기 파일을 불러옵니다. 
+        file_name = QFileDialog.getOpenFileName(self, "파일 열기 . . .", "", "jpg 파일(*.jpg);;png 파일", "jpg 파일(*.jpg)") #파일 확장자명 설정
+        #레이블에 띄웁니다. 
+        #opencv 는 파일 입출력 할때 아스키 문자만 허용한다. 절대 경로사용 불가
+        self.img_original = cv2.imdecode(np.fromfile(file_name[0], dtype=np.uint8),cv2.IMREAD_UNCHANGED)
+        if self.widget_cnt > 0:
+            dock.close()
+
+    def show_img_original(self):
+        global dock
+        self.widget_cnt = self.widget_cnt+1
+        # dock 위젯 만든다.  
+        dock = QDockWidget('원 본',self)
+        img_widget = ImageWidget(self)
+        #img_widget.set_image(self.img_original) #원본으로 설정
+        # adding widget to the layout
+        dock.setWidget(img_widget)
+        # setting geometry tot he dock widget
+        dock.setGeometry(100, 100, 200, 300)
+        dock.show()
+
+    def savefile(self): #파일 저장하기 
+        print('save file')     
+
+class ImageWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(ImageWidget, self).__init__(parent)
+        self.image_frame = QLabel() #이미지 프레임 생성
+        self.image = np.zeros((3,3),np.uint8)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.image_frame)
+        self.setLayout(self.layout)
+        self.set_image(parent.img_original)
+
+    def set_image(self,img): #이미지 변경
+        print('a')
+        self.image = img
+        self.show_image()
+
+    @pyqtSlot()
+    def show_image(self):
+        self.image = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+        self.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.image))
+
 if __name__ == '__main__': #실행이 main함수인 경우
     app = QApplication(sys.argv)
     ex = MyApp()
